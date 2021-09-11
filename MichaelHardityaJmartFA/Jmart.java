8a99607b7b42fa3f8d20d46455d2efa3c1d5f96a
Michael Harditya <michaelharditya@gmail.com>
2021-09-11 10:13:30 +0700

    Adding getDiscountedPrice method and getOriginalPrice method

--- MichaelHardityaJmartFA/Jmart.java
+++ MichaelHardityaJmartFA/Jmart.java
@@ -28,4 +28,19 @@ public class Jmart
         }
         return disc;
     }
+        public int getDiscountedPrice(int price, float discountPercentage){
+        if (discountPercentage < 100 && discountPercentage > 0){
+            price -= (price * discountPercentage);
+	}
+	else{
+	    price = 0;
+        }
+	return price;
+    }
+    public int getOriginalPrice(int discountedPrice, float discountPercentage){
+        int price;
+        discountPercentage = 100 - discountPercentage;
+        price = discountedPrice + (int)(discountedPrice/discountPercentage);
+        return price;
+    }
 }
