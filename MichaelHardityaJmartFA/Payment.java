c4a5887edb2486985cbba3accc1f830d1ee8b339
Michael Harditya <michael.harditya@ui.ac.id>
2021-09-25 10:31:28 +0700

    Added Payment class

---
+++ MichaelHardityaJmartFA/Payment.java
@@ -0,0 +1,33 @@
+package MichaelHardityaJmartFA;
+
+
+public class Payment extends Transaction implements FileParser
+{
+    public int productId;
+    public ShipmentDuration shipmentDuration;
+    
+    public Payment(int id,int buyerId,Product product,ShipmentDuration shipmentDuration)
+    {
+        super(id,buyerId,product.id);
+        this.productId = product.id;
+        this.shipmentDuration = shipmentDuration;
+    }
+    public Payment(int id,int buyerId,int storeId,int productId,ShipmentDuration shipmentDuration)
+    {
+        super(id,buyerId,productId);
+        this.productId = productId;
+        this.shipmentDuration = shipmentDuration;
+    }
+    public boolean validate()
+    {
+        return false;
+    }
+    public Transaction perform()
+    {
+        return null;
+    }
+    public boolean read(String content)
+    {
+        return false;
+    }
+}
