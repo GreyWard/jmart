f4fd66fed54f1f5439d288ff84846cba41112edb
Michael Harditya <michael.harditya@ui.ac.id>
2021-09-25 09:53:35 +0700

    Added interfacce FileParser

---
+++ MichaelHardityaJmartFA/FileParser.java
@@ -0,0 +1,13 @@
+package MichaelHardityaJmartFA;
+
+
+public interface FileParser
+{
+    public boolean read(String content);
+    public default Object write(){
+        return null;
+    }
+    public static Object newInstance(String content){
+        return null;
+    }
+}
