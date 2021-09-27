766f2a5f28d23b819c3cd36c29d5b368b2159adc
Michael Harditya <michael.harditya@ui.ac.id>
2021-09-27 20:44:17 +0700

    PT modul 3 - Cleared unnecessary method in Jmart

--- MichaelHardityaJmartFA/Jmart.java
+++ MichaelHardityaJmartFA/Jmart.java
@@ -12,17 +12,4 @@ public class Jmart
     public static void main(String[] args){
 
     }
-    /*public static Product createProduct(){
-        PriceTag price = new PriceTag(10000,5);
-        Product testcase = new Product("Primogem",1,true,price,ProductCategory.GAMING);
-        return testcase;
-    }
-    public static Coupon createCoupun(){
-        Coupon testcase = new Coupon("promo 11", 11, Coupon.Type.DISCOUNT,11,0.11);
-        return testcase;
-    }
-    public static ShipmentDuration createShipmentDuration(){
-        ShipmentDuration testcase = new ShipmentDuration(ShipmentDuration.INSTANT, ShipmentDuration.REGULER, ShipmentDuration.SAME_DAY);
-        return testcase;
-    }*/
 }
