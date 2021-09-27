8e526cb212f4a6163e6f57090049309848c22225
Michael Harditya <michael.harditya@ui.ac.id>
2021-09-27 19:59:01 +0700

    Moved Complaint parent to Recognizable

--- MichaelHardityaJmartFA/Complaint.java
+++ MichaelHardityaJmartFA/Complaint.java
@@ -1,37 +1,18 @@
 package MichaelHardityaJmartFA;
 /**menyimpan data complain
    *@author Michael Harditya*/
-public class Complaint extends Transaction implements FileParser
+public class Complaint extends Recognizable implements FileParser
 {
-    public int paymentId;
+    public String date;
     public String desc;
    /**@param id berisikan id transaksi {@code Complaint}
    *@param payment berisikan id pembeli ({@code Payment.buyerId}), id toko ({@code Payment.storeId}) dan id pembayaran ({@code Payment.id})
    *@param desc berisikan deskripsi komplain {@code Complaint}*/
-    public Complaint(int id, Payment payment, String desc)
+    public Complaint(int id, String desc)
     {
-        super(id,payment.buyerId,payment.storeId);
-        this.paymentId = payment.id;
+        super(id);
+        this.date = "Melintasi waktu dan tempat";
         this.desc = desc;
-    }
-   /**@param id berisikan id transaksi {@code Complaint}
-   *@param buyerId berisikan id pembeli {@code Complaint}
-   *@param storeId berisikan id toko {@code Complaint}
-   *@param paymentId berisikan id pembayaran {@code Complaint}
-   *@param desc berisikan deskripsi komplain {@code Complaint}*/
-    public Complaint(int id, int buyerId, int storeId, int paymentId, String desc)
-    {
-        super(id,buyerId,storeId);
-        this.paymentId = paymentId;
-        this.desc = desc;
-    }
-    public boolean validate()
-    {
-        return false;
-    }
-    public Transaction perform()
-    {
-        return null;
     }
     public boolean read(String content)
     {
