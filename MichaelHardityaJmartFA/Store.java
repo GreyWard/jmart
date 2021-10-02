d8d94905a06a8882b01d7d53c51bb141b1ae0ece
Michael Harditya <michael.harditya@ui.ac.id>
2021-10-02 10:59:18 +0700

    CS modul 4 - Fixed REGEX_NAME

--- MichaelHardityaJmartFA/Store.java
+++ MichaelHardityaJmartFA/Store.java
@@ -3,7 +3,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class Store extends Recognizable implements FileParser
 {
-    public static final String REGEX_NAME = "[A-Z]*{3,19}[ ]!{2}";
+    public static final String REGEX_NAME = "[A-Z][a-zA-Z0-9]{3,19}[ ]{1}?";
     public static final String REGEX_PHONE = "[0-9]{9,12}";
     public String name;
     public String address;
