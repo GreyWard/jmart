41a179c90e4fb6205cb65aa208e79ff1eddc9c44
Michael Harditya <michael.harditya@ui.ac.id>
2021-10-09 11:05:53 +0700

    CS modul 5 - Added min and max

--- MichaelHardityaJmartFA/Algorithm.java
+++ MichaelHardityaJmartFA/Algorithm.java
@@ -66,5 +66,20 @@ public class Algorithm{
 		}
 		return null;
 	}
-	//public static <T> T max
+	public static <T extends Comparable<? super T>> T max(T first, T second) {
+		if (first.compareTo(second)<0) {
+			return first;
+		}
+		else {
+			return second;
+		}
+	}
+	public static <T extends Comparable<? super T>> T min(T first, T second) {
+		if (second.compareTo(first)<0) {
+			return first;
+		}
+		else {
+			return second;
+		}
+	}
 }
