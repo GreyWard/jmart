5062ccb2618a3bebd995fda6b017b45fe45b8894
Michael Harditya <michael.harditya@ui.ac.id>
2021-10-09 10:56:24 +0700

    CS modul 5 - Added find and count method in Algorithm class

--- MichaelHardityaJmartFA/Algorithm.java
+++ MichaelHardityaJmartFA/Algorithm.java
@@ -19,6 +19,10 @@ public class Algorithm{
 		Predicate<T> pred = value::equals;
 		return count(iterator,pred);
 	}
+	public static <T> int count(T[] array,Predicate<T> pred) {
+		Iterator<T> iterator = Arrays.stream(array).iterator();
+		return count(iterator,pred);
+	}
 	public static <T> int count(Iterable<T> iterable, Predicate<T> pred) {
 		Iterator<T> iterator = iterable.iterator();
 		return count(iterator,pred);
@@ -31,6 +35,27 @@ public class Algorithm{
 			}
 		}
 		return counter;
+	}
+	public static <T> int find(T[] array,T value) {
+		Iterator<T> iterator = Arrays.stream(array).iterator();
+		Predicate<T> pred = value::equals;
+		return count(iterator,pred);
+	}
+	public static <T> int find(Iterable<T> iterable, T value) {
+		Predicate<T> pred = value::equals;
+		return count(iterable, pred);
+	}
+	public static <T> int find(Iterator<T> iterator, T value) {
+		Predicate<T> pred = value::equals;
+		return count(iterator,pred);
+	}
+	public static <T> int find(T[] array,Predicate<T> pred) {
+		Iterator<T> iterator = Arrays.stream(array).iterator();
+		return count(iterator,pred);
+	}
+	public static <T> int find(Iterable<T> iterable, Predicate<T> pred) {
+		Iterator<T> iterator = iterable.iterator();
+		return count(iterator,pred);
 	}
 	public static <T> T find(Iterator<T> iterator, Predicate<T> pred) {
 		while(iterator.hasNext()) {
