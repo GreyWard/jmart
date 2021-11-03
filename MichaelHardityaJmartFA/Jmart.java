2133fa9114015bbbeedf91d9b68af175406765ac
Michael Harditya <michael.harditya@ui.ac.id>
2021-11-03 21:00:53 +0700

    TP modul 6 - Changed Jmart to test import for json files

--- MichaelHardityaJmartFA/Jmart.java
+++ MichaelHardityaJmartFA/Jmart.java
@@ -1,15 +1,40 @@
 package MichaelHardityaJmartFA;
 
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
 /**
  * a JMart Apps for managing a store
  *
  * @author Michael Harditya
  * @version 0.0.1
  */
-public class Jmart
+class Jmart
 {
+	class Country
+	{
+		public String name;
+		public int population;
+		public List<String> listOfStates;
+	}
+	
     public static void main(String[] args){
-
+    	String filepath = "D:/JavaProject/jmart/lib/city.json";
+    	Gson gson = new Gson();
+    	try
+    	{
+    		BufferedReader br = new BufferedReader(new FileReader(filepath));
+    		Country input = gson.fromJson(br,Country.class);
+    		System.out.println("name: " + input.name);
+    		System.out.println("population: " + input.population);
+    		System.out.println("states:");
+    		input.listOfStates.forEach(state -> System.out.println(state));
+    	}
+    	catch (IOException e)
+    	{
+    		e.printStackTrace();
+    	}
     }
 }
