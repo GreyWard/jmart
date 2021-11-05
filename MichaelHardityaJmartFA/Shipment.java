2ed5405ce84b2a8f69923b52f9ff17fa6c39204c
Michael Harditya <michael.harditya@ui.ac.id>
2021-11-05 21:04:04 +0700

    PreCS modul 6 - Simplified Shipment class

--- MichaelHardityaJmartFA/Shipment.java
+++ MichaelHardityaJmartFA/Shipment.java
@@ -12,66 +12,65 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 public class Shipment
 {
+	public static Plan INSTANT = new Plan((byte)(1 << 0));
+    public static Plan SAME_DAY = new Plan((byte)(1 << 1));
+    public static Plan NEXT_DAY = new Plan((byte)(1 << 2));
+    public static Plan REGULER = new Plan((byte)(1 << 3));
+    public static Plan KARGO = new Plan((byte)(1 << 4));
+    public static final SimpleDateFormat ESTIMATION_FORMAT = new SimpleDateFormat("dd/MM/DD/yyyy");
     public String address;
-    public int shipmentCost;
-    public Duration duration;
+    public int cost;
     public String receipt;
+    public byte plan;
     /**
      * Constructor for objects of class Shipment
      */
-    public Shipment(String address, int shipmentCost, Duration duration, String receipt)
+    public Shipment(String address, int cost, byte plan, String receipt)
     {
         this.address = address;
-        this.shipmentCost = shipmentCost;
-        this.duration = duration;
+        this.cost = cost;
+        this.plan = plan;
         this.receipt = receipt;
-    }
-    public boolean read(String content)
-    {
-        return false;
-    }
-    public static class Duration
-    {
-    public static Duration INSTANT = new Duration((byte)(1 << 0));
-    public static Duration SAME_DAY = new Duration((byte)(1 << 1));
-    public static Duration NEXT_DAY = new Duration((byte)(1 << 2));
-    public static Duration REGULER = new Duration((byte)(1 << 3));
-    public static Duration KARGO = new Duration((byte)(1 << 4));
-    public static final SimpleDateFormat ESTIMATION_FORMAT = new SimpleDateFormat("dd/MM/DD/yyyy");
-    public byte bit;
-    private Duration(byte bit){
-        this.bit = bit;
     }
     public String getEstimatedArrival(Date reference){
         Calendar now = Calendar.getInstance();
         now.setTime(reference);
-        if (bit == 2){
+        if (plan == 2){
             now.add(Calendar.DATE,1);
         }
-        else if(bit == 3){
+        else if(plan == 3){
             now.add(Calendar.DATE,2);
         }
-        else if(bit == 4){
+        else if(plan == 4){
             now.add(Calendar.DATE,5);            
         }
-        return Shipment.Duration.ESTIMATION_FORMAT.format(now.getTime());
+        return Shipment.ESTIMATION_FORMAT.format(now.getTime());
     }
-}
-    public class MultiDuration
-    {
-        public byte bit;
-        public MultiDuration(Duration... args){
-        for (Duration i:args){
-            this.bit |= i.bit;
-        }
-        }
-    public boolean isDuration(Duration reference){
-        if ((this.bit & reference.bit) == reference.bit){
+    public boolean isDuration(Plan reference){
+        if ((this.plan & reference.bit) == reference.bit){
             return true;
         }
         else{
             return false;
         }
     }
+    public static boolean isDuration(byte object, Plan reference){
+        if ((object & reference.bit) == reference.bit){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
+    public boolean read(String content)
+    {
+        return false;
+    }
+    public static class Plan
+    {
+    public final byte bit;
+    private Plan (byte bit){
+        this.bit = bit;
+    }
+}
 }
