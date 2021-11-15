00cded97e0adbf16d63a199e60ac37636b0d7027
Michael Harditya <michael.harditya@ui.ac.id>
2021-11-15 20:29:41 +0700

    PT modul 7 - Added run method for threading

--- MichaelHardityaJmartFA/ObjectPoolThread.java
+++ MichaelHardityaJmartFA/ObjectPoolThread.java
@@ -2,23 +2,36 @@ package MichaelHardityaJmartFA;
 import java.util.*;
 import java.util.function.Function;
 public class ObjectPoolThread<T> extends Thread {
-	private boolean exitSignal;
+	private boolean exitSignal = false;
 	private Vector<T> objectPool;
 	private Function<T,Boolean> routine;
 	public ObjectPoolThread (String name, Function<T,Boolean> routine) {
+		Thread thread = new Thread(name);
+		thread.start();
 		this.routine = routine;
 	}
 	public ObjectPoolThread(Function<T,Boolean> routine) {
-		this.routine = routine;
+
 	}
-	public void add (T object) {
+	public synchronized void add (T object) {
 		objectPool.add(object);
 	}
-	public void exit() {
+	public synchronized void exit() {
 		this.exitSignal = true;
 	}
 	public void run() {
-		
+		try {
+		for (T iterate : objectPool) {
+			if (routine.apply(iterate)) {
+				Thread.sleep(50);
+			}
+			if (exitSignal == true) {
+				break;
+			}
+		}
+		}catch (InterruptedException e) {
+			System.out.println("Thread interrupted.");
+		}
 	}
 	public int size() {
 		return objectPool.size();
