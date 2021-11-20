4a4b4059a87cc8b2beb0fd4165845bf8d75a1572
Michael Harditya <michael.harditya@ui.ac.id>
2021-11-20 11:06:14 +0700

    CS modul 8 - fixed PaymentController types

--- src/main/java/com/MichaelHardityaJmartFA/controller/PaymentController.java
+++ src/main/java/com/MichaelHardityaJmartFA/controller/PaymentController.java
@@ -5,9 +5,9 @@ import com.MichaelHardityaJmartFA.Payment;
 import com.MichaelHardityaJmartFA.dbjson.JsonAutowired;
 import com.MichaelHardityaJmartFA.dbjson.JsonTable;
 
-public class PaymentController implements BasicGetController<Account>
+public class PaymentController implements BasicGetController<Payment>
 {
-	@JsonAutowired(filepath = "a/b/payment.json", value = Account.class) 
+	@JsonAutowired(filepath = "a/b/payment.json", value = Payment.class) 
     public static JsonTable<Payment> paymentTable;
 	public JsonTable getJsonTable() {
 		return paymentTable;
